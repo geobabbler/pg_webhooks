c2d1841e36ad4235c13e39b4f9c76eb3608d94a3
William Dollins <bill@geomusings.com>
2023-07-10 16:50:40 -0400

    Added subscribe logic

---
+++ routes/subscriptions.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+//import { Pool, Client } from 'pg';
+var dotenv = require('dotenv').config();
+const URL = require("url").URL;
+const url = require("url");
+
+//var dbUrl = 'tcp://postgres:psw@localhost:5432/test-db';
+
+const stringIsAValidUrl = (s) => {
+  try {
+    var ss = new URL(s.toLowerCase());
+    const protocols = ['http:', 'https:'];
+    return protocols.includes(ss.protocol);
+  } catch (err) {
+    return false;
+  }
+};
+
+/* subscribe to a channel */
+router.post('/add', async function (req, res, next) {
+  res.type('application/json');
+  var obj
+  try {
+    obj = eval(req.body);
+    if (obj.channel && obj.callback) {
+      if (stringIsAValidUrl(obj.callback)) {
+        urlhost = new URL(obj.callback).host.toLowerCase();
+        const client = new pg.Client({
+          user: process.env.PGUSER,
+          host: process.env.PGSERVER,
+          database: process.env.PGDATABASE,
+          password: process.env.PGPASS,
+          port: 5432,
+        })
+        await client.connect();
+        var queryString = `INSERT INTO public.subscriptions (channel, callback, host) VALUES('${obj.channel.toLowerCase()}', '${obj.callback.toLowerCase()}', '${urlhost}') returning *;`
+        var query = client.query(queryString, function (error, result) {
+          if (!error){
+          var retVal = {"subscription_id": result.rows[0].resource_id}
+          res.send(retVal)
+          }
+          else{
+            res.status(400);
+            res.send('Only one subscription per channel per host is allowed.')
+          }
+        });
+      }
+      else {
+        res.status(400);
+        res.send('Callback is not a valid url');
+      }
+    }
+    else {
+      res.status(400);
+      res.send('Channel not specified')
+    }
+  }
+  catch (e) {
+    res.status(400);
+    res.send("Content does not appear to be valid JSON")
+  }
+});
+
+/* unsubscribe from a channel */
+router.post('/remove', function (req, res, next) {
+  res.type('application/json');
+  res.send('remove a subscription');
+});
+
+module.exports = router;
